d3b278da04dbf5cb02e0ac055ee093a600653ebf
Hinrick <hinrick71@gmail.com>
2024-09-12 11:42:42 +0800

    first commit

---
+++ _next/static/3Lvc0zc2_kHia0gBkdjDt/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002Fc1","\u002Fc1\u002F[news_id]","\u002Fcsr_company_list\u002F[spc_id]","\u002Fmessage_director","\u002Fnews","\u002Fnews\u002F[news_id]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
